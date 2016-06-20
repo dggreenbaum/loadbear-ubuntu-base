d682dc7dc7b01286f651bde7c7253d70ea06a7be
Douglas Greenbaum <dggreenbaum@greenbad.org>
2016-06-19 19:56:12 -0700

    Cleaning up provisioning scripts

--- scripts/base.sh
+++ scripts/base.sh
@@ -3,18 +3,5 @@
 # etc., and remove optional things to trim down the machine.
 apt-get -y update
 apt-get -y upgrade
-apt-get -y install gcc build-essential linux-headers-$(uname -r)
-apt-get -y install zlib1g-dev libssl-dev libreadline-gplv2-dev libyaml-dev
-apt-get -y install vim curl
+apt-get -y install gcc build-essential linux-headers-$(uname -r) zlib1g-dev libssl-dev libreadline-gplv2-dev libyaml-dev vim curl nfs-common
 apt-get clean
-
-# Set up sudo
-#( cat <<'EOP'
-#%vagrant ALL=NOPASSWD:ALL
-#EOP
-#) > /tmp/vagrant
-#chmod 0440 /tmp/vagrant
-#mv /tmp/vagrant /etc/sudoers.d/
-
-# Install NFS client
-apt-get -y install nfs-common
